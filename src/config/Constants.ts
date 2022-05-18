f20c0c9c5371fe4d9b0120a72428537316e73cfe
Mario Rocha <eng.mec.mario.rocha@gmail.com>
2022-05-18 20:40:11 -0300

    v1

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: 'https://mario-dio-url-shortener.herokuapp.com/',
+    API_URL: 'https://mario-dio-url-shortener.herokuapp.com/' || 'http://localhost:5000',
     MONGO_CONNECTION:
         'mongodb+srv://mariorocha1997:XMGZr69HZHfkaQJ5@url-shortener-dio-mario.iup0g.mongodb.net/?retryWrites=true&w=majority'
 }
