06721b6a9a32b0e0b5c4128414c093f57956f0c1
Mario Rocha <eng.mec.mario.rocha@gmail.com>
2022-05-18 22:00:59 -0300

    fix v6

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,6 @@ api.use(express.json())
 
 const port = process.env.PORT || 5000;
 
-
-
 const database = new MongoConnection()
 database.connect()
 
@@ -19,7 +17,6 @@ const urlController = new URLController()
 api.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 api.post('/shorten', urlController.shorten)
 api.get('/:hash', urlController.redirect)
-
 
 api.listen(port, () => console.log('Express listening'))
 
