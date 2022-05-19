4dd2eb58d7e340ceaaf92415d5ec89d79fefab3a
Mario Rocha <eng.mec.mario.rocha@gmail.com>
2022-05-18 23:11:24 -0300

    release documentation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# url-shortener-dio
+# Encurtador de URL (URL Shortener)
+
+Esse é o repositório do desafio "Construindo Encurtador de URL" do Módulo V| - NodeJS do Bootcamp Spread Fullstack Developer
+
+Deploy: https://urldio.herokuapp.com/api-docs/
+
+
--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: 'https://urldio.herokuapp.com',
+    API_URL: 'https://urldio.herokuapp.com', //for localhost api run use 'https://localhost:5000'
     MONGO_CONNECTION:
         'mongodb+srv://mariorocha1997:XMGZr69HZHfkaQJ5@url-shortener-dio-mario.iup0g.mongodb.net/?retryWrites=true&w=majority'
 }
