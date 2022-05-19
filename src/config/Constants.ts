480955a6705115c6893475b678d6dd0af24d5c0d
Mario Rocha <eng.mec.mario.rocha@gmail.com>
2022-05-18 21:17:31 -0300

    fix start v4

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: 'https://urldio.herokuapp.com/',
+    API_URL: 'https://urldio.herokuapp.com',
     MONGO_CONNECTION:
         'mongodb+srv://mariorocha1997:XMGZr69HZHfkaQJ5@url-shortener-dio-mario.iup0g.mongodb.net/?retryWrites=true&w=majority'
 }
